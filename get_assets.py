626b6d9564ad1525fb7c4f513862e5573d085739
Michael <michaelwulu@gmail.com>
2023-09-27 22:39:33 +0200

    updated paths

--- get_assets.py
+++ get_assets.py
@@ -6,10 +6,10 @@ def get_summoner_icon(profileIconId):
     request = f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/{profileIconId}.jpg"
     response = requests.get(request)
 
-    if not os.path.exists(f"MasteryManager/temp/icons"):
-        os.makedirs(f"MasteryManager/temp/icons")
+    if not os.path.exists(f"temp/icons"):
+        os.makedirs(f"temp/icons")
 
-    with open(f"MasteryManager/temp/icons/icon{profileIconId}.jpg", "wb") as f:
+    with open(f"temp/icons/icon{profileIconId}.jpg", "wb") as f:
         f.write(response.content)
 
 
@@ -17,8 +17,8 @@ def get_champion_square(championId):
     request = f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-tiles/{championId}/{championId}000.jpg"
     response = requests.get(request)
 
-    if not os.path.exists(f"MasteryManager/temp/champions"):
-        os.makedirs(f"MasteryManager/temp/champions")
+    if not os.path.exists(f"temp/champions"):
+        os.makedirs(f"temp/champions")
 
-    with open(f"MasteryManager/temp/champions/champ{championId}.jpg", "wb") as f:
+    with open(f"temp/champions/champ{championId}.jpg", "wb") as f:
         f.write(response.content)
